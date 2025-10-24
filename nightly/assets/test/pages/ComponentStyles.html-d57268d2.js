01b3e10aad8d3cd1ef4ae1afc2399c690cb1d29f
NHristov-sap <NHristov-sap@users.noreply.github.com>
2025-10-24 01:09:32 +0000

    Deploying to gh-pages from @ NHristov-sap/ui5-webcomponents@55d99d25324f64c202339c4947f2b3bcf35e25c8 🚀

---
+++ nightly/assets/test/pages/ComponentStyles.html-d57268d2.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-6271ca58.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-53d050a8.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-9a0e13b0.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
