bed1cc2618292bd0fc364e0974f8964918114f31
NHristov-sap <NHristov-sap@users.noreply.github.com>
2026-06-13 02:55:28 +0000

    Deploying to gh-pages from @ NHristov-sap/ui5-webcomponents@55d99d25324f64c202339c4947f2b3bcf35e25c8 🚀

--- nightly/assets/test/pages/ComponentStyles.html-dbf00ec4.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-202fb2aa.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1781233147,
+    buildTime: 1781319100,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
