20aaab994824ddef58caf6dad402bc6a38963787
NHristov-sap <NHristov-sap@users.noreply.github.com>
2026-07-06 02:43:02 +0000

    Deploying to gh-pages from @ NHristov-sap/ui5-webcomponents@55d99d25324f64c202339c4947f2b3bcf35e25c8 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1783218880,
+    buildTime: 1783305545,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
