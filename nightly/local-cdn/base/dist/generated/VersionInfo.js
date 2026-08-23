45ed93e2c4bb7ffafd965ec9b884044b89f719f4
NHristov-sap <NHristov-sap@users.noreply.github.com>
2026-08-23 01:02:57 +0000

    Deploying to gh-pages from @ NHristov-sap/ui5-webcomponents@55d99d25324f64c202339c4947f2b3bcf35e25c8 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1787360105,
+    buildTime: 1787446732,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
---
+++ nightly/local-cdn/main/dist/assets/test/pages/ComponentStyles.html-b56f3c80.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-35581f09.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
